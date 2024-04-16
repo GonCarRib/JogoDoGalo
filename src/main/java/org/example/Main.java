a7c63fb7b66cf4c972b81acc5d3f4889149ae3a1
Otavio-A <otavio200100@hotmail.com>
2024-04-16 18:52:22 +0100

    posicoes sao preenchidas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,25 +5,29 @@ import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static String[][] jogo = {{"", "", ""}, {"", "", ""}, {"", "", ""}};
+    static String[][] jogo = {{"-", "-", "-"}, {"-", "-", "-"}, {"-", "-", "-"}};
     static String[][] posicoes = {{"0","1","2"},{"3","4","5"},{"6","7","8"}};
     static boolean suavez;
     static int size = 2;
     static Scanner myObj = new Scanner(System.in);
+    static int rown, coln;
 
     public static void main(String[] args){
         doisJogadores(jogo, posicoes, myObj);
     }
 
     static void doisJogadores(String[][] arr, String[][] pos, Scanner scan){
-        while(suavez){
-            printarr(pos);
-            printarr(arr);
-            int jogada = scan.nextInt();
-            if (!jaExiste()){
-                jogo.
-            }
+        printarr(pos);
+        printarr(arr);
+        while(!suavez){
 
+            int jogada = scan.nextInt();
+            encPos(jogada);
+            if (!jaExiste()){
+                jogo[rown][coln] = "X";
+                printarr(pos);
+                printarr(arr);
+            }else
         }
     }
     static void printarr(String[][] arr) {
@@ -35,14 +39,21 @@ public class Main {
         }
     }
     public static boolean jaExiste() {
-        for (int i = 0; i <= 2; ++i) {
-            for (int j = 0; j <= 2; ++j) {
-                if (jogo[i][j].isEmpty()) {
+                if (jogo[rown][coln].equals("-")) {
                     return false;
+            }
+
+        System.out.println("Numero ja foi selecionado");
+        return true;
+    }
+    public static void encPos(int x) {
+        for (int i = 0; i <= size; ++i) {
+            for (int j = 0; j <= size; ++j) {
+                if (posicoes[i][j].equals(String.valueOf(x))) {
+                    rown = i;
+                    coln = j;
                 }
             }
         }
-        System.out.println("Numero ja foi selecionado");
-        return true;
     }
 }
