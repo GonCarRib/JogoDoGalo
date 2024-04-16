a6db16a868d620d025cace51b25db76328694ffa
Otavio-A <otavio200100@hotmail.com>
2024-04-16 18:32:09 +0100

    ta dificil

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,48 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    static String[][] jogo = {{"", "", ""}, {"", "", ""}, {"", "", ""}};
+    static String[][] posicoes = {{"0","1","2"},{"3","4","5"},{"6","7","8"}};
+    static boolean suavez;
+    static int size = 2;
+    static Scanner myObj = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void main(String[] args){
+        doisJogadores(jogo, posicoes, myObj);
+    }
+
+    static void doisJogadores(String[][] arr, String[][] pos, Scanner scan){
+        while(suavez){
+            printarr(pos);
+            printarr(arr);
+            int jogada = scan.nextInt();
+            if (!jaExiste()){
+                jogo.
+            }
+
         }
+    }
+    static void printarr(String[][] arr) {
+        for (int i = 0; i <= size; ++i) {
+            for (int j = 0; j <= size; ++j) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    public static boolean jaExiste() {
+        for (int i = 0; i <= 2; ++i) {
+            for (int j = 0; j <= 2; ++j) {
+                if (jogo[i][j].isEmpty()) {
+                    return false;
+                }
+            }
+        }
+        System.out.println("Numero ja foi selecionado");
+        return true;
     }
 }
